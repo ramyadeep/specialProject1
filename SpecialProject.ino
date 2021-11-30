0e7841ca798b9f86f54c5ea349f7389298d23094
= <=>
2021-11-30 19:47:47 +0530

    disabled brownout detector

--- SpecialProject.ino
+++ SpecialProject.ino
@@ -1,7 +1,8 @@
-//#define SensorPin A0  // used for Arduino and ESP8266
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
-#define moisturePin 4  // used for ESP32
-#define smokePin 5     // used for ESP32
+#define moisturePin 4 //A0  //4  // used for ESP32
+#define smokePin 5 //A1    //5  // used for ESP32
 
 
 // Timings
@@ -13,21 +14,24 @@ const long moistureInterval = 5000;
 float sensorValue;
 
 void setup() { 
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
   Serial.begin(9600);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis1 >= smokeInterval) {
-    previousMillis1 = currentMillis;
-    sensorValue = analogRead(smokePin);
-    Serial.print("Smoke level: ");
-    Serial.print(sensorValue);
-  }
   if (currentMillis - previousMillis2 >= moistureInterval) {
     previousMillis2 = currentMillis;
     sensorValue = analogRead(moisturePin);
     Serial.print("moisture level: ");
+    Serial.print(sensorValue);
+  }
+  if (currentMillis - previousMillis1 >= smokeInterval) {
+    previousMillis1 = currentMillis;
+    sensorValue = analogRead(smokePin);
+    Serial.print(" Smoke level: ");
     Serial.println(sensorValue);
   }
+  
+
 } 
