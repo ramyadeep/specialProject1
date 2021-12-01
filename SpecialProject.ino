38217daa4957a08b4869da2bee4343a0fab2ca93
= <=>
2021-12-01 11:22:56 +0530

    Sensor Interface complete

--- SpecialProject.ino
+++ SpecialProject.ino
@@ -1,8 +1,8 @@
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
+//#include "soc/soc.h"
+//#include "soc/rtc_cntl_reg.h"
 
-#define moisturePin 4 //A0  //4  // used for ESP32
-#define smokePin 5 //A1    //5  // used for ESP32
+#define moisturePin A1  //4  // used for ESP32
+#define smokePin A2    //5  // used for ESP32
 
 
 // Timings
@@ -14,7 +14,7 @@ const long moistureInterval = 5000;
 float sensorValue;
 
 void setup() { 
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+//  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
   Serial.begin(9600);
 }
 
@@ -22,15 +22,17 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis2 >= moistureInterval) {
     previousMillis2 = currentMillis;
-    sensorValue = analogRead(moisturePin);
+    sensorValue = map(analogRead(moisturePin),0,1024,0,100);
     Serial.print("moisture level: ");
     Serial.print(sensorValue);
+    Serial.print(" %");
   }
   if (currentMillis - previousMillis1 >= smokeInterval) {
     previousMillis1 = currentMillis;
-    sensorValue = analogRead(smokePin);
+    sensorValue = map(analogRead(smokePin),0,1024,0,100);
     Serial.print(" Smoke level: ");
-    Serial.println(sensorValue);
+    Serial.print(sensorValue);
+    Serial.println(" %");
   }
   
 
